7c8f279473ed98e5c36c57d5ac805b469656d72e
Bryan Martinez Zavala <brymartinezzavala@wl.k12.ia.us>
2018-11-19 14:49:16 -0600

    Fixed repo

---
+++ js/teachers.js
@@ -0,0 +1,33 @@
+var teachers = [
+     
+    {
+        image: "brown.jpg",
+        name: "Brown",
+        subjects: "Special Education",
+        teacher_id: "0001",
+    },
+    {
+        image: "gunn.jpg",
+        name: "Gunn",
+        subjects: "Science",
+        teacher_id: "0002",
+    },
+    {
+        image: "buckley.jpg",
+        name: "Buckley",
+        subjects: "Math",
+        teacher_id: "0003",
+    },
+    { 
+        image: "hauschildt.jpg",
+        name: "Hauschildt",
+        subjects: "Business, Computer Science, Track (HS) Assistant Coach, Yearbook Adviser",
+        teacher_id: "0004",
+    },
+    {
+        image: "czarnecki.jpg",
+        name: "Czarnecki",
+        subjects: "Teacher, Athletics",
+        teacher_id: "0005",
+    },
+];
